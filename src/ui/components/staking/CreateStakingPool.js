edaabf28f1188128809e317c52273a36197d8a20
andriy-bs <andriy.beskyd@gmail.com>
2022-11-07 13:18:51 +0200

    Saving the key pair to a zip-file. Password protection

--- src/ui/components/staking/CreateStakingPool.js
+++ src/ui/components/staking/CreateStakingPool.js
@@ -8,17 +8,24 @@ import Typography from "@mui/material/Typography";
 import {InputAdornment} from "@mui/material/index";
 import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
+import * as zip from "@zip.js/zip.js";
 
 
 const CreateStakingPool = ({ wallet }) => {
 	const [poolName, setPoolName] = React.useState('');
 	const [ownerAccount, setOwnerAccount] = React.useState('');
 	const [publicKey, setPublicKey] = React.useState('');
+	const [password, setPassword] = React.useState('');
+	const [confirmPassword, setConfirmPassword] = React.useState('');
+	const [keyPair, setKeyPair] = React.useState(null);
 	const [percentageFee, setPercentageFee] = React.useState('');
 	const [poolNameErrorText, setPoolNameErrorText] = React.useState('');
 	const [ownerAccountErrorText, setOwnerAccountErrorText] = React.useState('');
 	const [publicKeyErrorText, setPublicKeyErrorText] = React.useState('');
 	const [percentageFeeErrorText, setPercentageFeeErrorText] = React.useState('');
+	const [keyPairErrorText, setKeyPairErrorText] = React.useState('');
+	const [confirmPasswordErrorText, setConfirmPasswordErrorText] = React.useState('');
+	const [isKeyPairDownloaded, setIsKeyPairDownloaded] = React.useState(true);
 
 
 	async function submitCreateStakingPool(e) {
@@ -31,24 +38,83 @@ const CreateStakingPool = ({ wallet }) => {
 	const handlePublicKeyChange = event => {
 		setPublicKey(event.target.value);
 		setPublicKeyErrorText('');
+		setKeyPair(null);
 	};
 
 	const handleGenerateKey = () => {
 		if (poolName) {
 			const kp = generateKey(poolName + '.' + process.env.REACT_APP_CONTRACT_POOL);
+			setKeyPair(kp);
 			setPublicKey(kp.public_key);
+			setIsKeyPairDownloaded(false);
+			setPublicKeyErrorText('');
 		} else {
 			setPoolNameErrorText('Please enter Pool Name');
 		}
 	};
 
 	const chekForm = () => {
-		if (!poolName) setPoolNameErrorText('Please enter Pool Name');
-		if (!ownerAccount) setOwnerAccountErrorText('Please enter Owner account');
-		if (!publicKey) setPublicKeyErrorText('Please enter Public key');
-		if (!percentageFee) setPercentageFeeErrorText('Please enter Percentage fee');
-		return poolName && ownerAccount && publicKey && percentageFee;
+		if (!poolName) {
+			setPoolNameErrorText('Please enter Pool Name');
+			return false;
+		}
+		if (!ownerAccount) {
+			setOwnerAccountErrorText('Please enter Owner account');
+			return false;
+		}
+		if (!publicKey) {
+			setPublicKeyErrorText('Please enter Public key');
+			return false;
+		}
+		if (!percentageFee) {
+			setPercentageFeeErrorText('Please enter Percentage fee');
+			return false;
+		}
+		if (keyPair && !isKeyPairDownloaded) {
+			setKeyPairErrorText('Please download Key Pair');
+			return false;
+		}
+		return true;
 	};
+
+	const getZipFileBlob = async (keyPair) => {
+		if (!comparePasswords()) return false;
+		const zipWriter = new zip.ZipWriter(new zip.BlobWriter("application/zip"));
+		await Promise.all([
+			zipWriter.add(poolName + '.' + process.env.REACT_APP_CONTRACT_POOL + '.json',
+				new zip.TextReader(JSON.stringify(keyPair, null, 2)),
+				{ password }
+			),
+		]);
+		return zipWriter.close();
+	}
+
+	const downloadFile = (blob) => {
+		if (!blob) return false;
+		const a = document.createElement('a');
+		a.download = poolName + '.' + process.env.REACT_APP_CONTRACT_POOL + '.zip';
+		a.href = URL.createObjectURL(blob);
+		a.addEventListener('click', (e) => {
+			setTimeout(() => URL.revokeObjectURL(a.href), 30 * 1000);
+		});
+		a.click();
+		setKeyPairErrorText('');
+		setIsKeyPairDownloaded(true);
+	}
+
+	const comparePasswords = () => {
+		if (!password || !confirmPassword) {
+			setConfirmPasswordErrorText('Please enter passwords');
+			return false;
+		}
+		if (password === confirmPassword) {
+			setConfirmPasswordErrorText('');
+			return true;
+		} else {
+			setConfirmPasswordErrorText('Passwords do not match');
+			return false;
+		}
+	}
 
 
 	return (
@@ -107,9 +173,49 @@ const CreateStakingPool = ({ wallet }) => {
 					helperText={ publicKeyErrorText }
 					error={ !!publicKeyErrorText }
 					value={ publicKey } onChange={ handlePublicKeyChange }/>
-				<Grid container justifyContent="flex-end">
-					<Link onClick={ handleGenerateKey } href="#">Don't have an Public key? Generate Key</Link>
-				</Grid>
+				{ keyPair ?
+					<>
+						<Grid container justifyContent="flex-end">
+							<Link onClick={ () => {
+								setKeyPair(null);
+								setPublicKey('');
+							} } href="#">I have an Key pair</Link>
+						</Grid>
+						<TextField
+							type="password"
+							margin="normal"
+							required
+							fullWidth
+							id="password"
+							label="Password"
+							autoComplete="off"
+							error={ !!confirmPasswordErrorText }
+							value={ password } onChange={ e => {
+							setPassword(e.target.value);
+						} }/>
+						<TextField
+							type="password"
+							margin="normal"
+							required
+							fullWidth
+							id="confirmPassword"
+							label="Confirm password"
+							autoComplete="off"
+							helperText={ confirmPasswordErrorText }
+							error={ !!confirmPasswordErrorText }
+							value={ confirmPassword } onChange={ e => {
+							setConfirmPassword(e.target.value);
+						} }/>
+						<Grid container justifyContent="flex-end">
+							<Box sx={ { color: 'error.main' } } p={ 1 }>{ keyPairErrorText }</Box>
+							<Button onClick={ () => getZipFileBlob(keyPair).then(downloadFile) } variant="outlined">Download Key
+								pair</Button>
+						</Grid>
+					</> :
+					<Grid container justifyContent="flex-end">
+						<Link onClick={ handleGenerateKey } href="#"> Generate Key pair</Link>
+					</Grid>
+				}
 				<TextField
 					type="number"
 					margin="normal"
