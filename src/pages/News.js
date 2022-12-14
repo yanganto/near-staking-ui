1f1ad6734d4ffb9b8bef8d62d359a88835954750
andriy-bs <andriy.beskyd@gmail.com>
2022-12-14 16:18:15 +0200

    #13 Add a 'News' section

---
+++ src/pages/News.js
@@ -0,0 +1,52 @@
+import {Box, Card, CardActions, CardContent, CardHeader, Container, Link, Typography} from "@mui/material";
+import parse from 'html-react-parser'
+import {useEffect, useState} from "react";
+import {useTheme} from '@mui/material/styles';
+
+
+const News = () => {
+	const [news, setNews] = useState([]);
+	const theme = useTheme();
+
+	useEffect(() => {
+		(async () => {
+			const res = await fetch('news.json').then(response => {
+				return response.json();
+			}).then(data => {
+				return (data);
+			}).catch(err => {
+				console.error("Error Reading data " + err);
+			});
+			setNews(res.reverse());
+		})();
+	}, []);
+
+
+	return <Container>
+		<Typography component="h1" variant="h5" align="center">
+			News
+		</Typography>
+		{
+			news.map((n, index) => (
+				<Box p={ 1 } key={ index }>
+					<Card variant="outlined">
+						<CardHeader title={ n.title } style={ {
+							backgroundColor: theme.palette.primary.main,
+							color: theme.palette.primary.contrastText
+						} }/>
+						<CardContent>
+							{ parse(n.description) }
+						</CardContent>
+						{ n.external_link ?
+							<CardActions>
+								<Link href={ n.external_link } target="_blank" rel="noreferrer">{ n.external_link }</Link>
+							</CardActions>
+							: null }
+					</Card>
+				</Box>
+			))
+		}
+	</Container>;
+};
+
+export default News;
