2918e47490d8a041786f25489998e5afe2dd3ab5
andriy-bs <andriy.beskyd@gmail.com>
2023-02-10 12:19:18 +0200

    Update pool rewards calc

--- backend/src/services/getMyRewards.js
+++ backend/src/services/getMyRewards.js
@@ -1,7 +1,7 @@
 import {mainnet_DelegationRewards, testnet_DelegationRewards} from "../models/mDelegationRewards.js";
 
 
-const getDRewards = async (network, account_id, group, dateFrom, dateTo) => {
+const getDRewards = async (network, account_id, pool, group, dateFrom, dateTo) => {
 	const DelegationRewards = network === 'mainnet' ? mainnet_DelegationRewards : testnet_DelegationRewards;
 	const dateTo2 = new Date(dateTo);
 	dateTo2.setUTCHours(0, 0, 0, 0);
@@ -10,7 +10,9 @@ const getDRewards = async (network, account_id, group, dateFrom, dateTo) => {
 	const dRewards = await DelegationRewards.aggregate([
 		{
 			"$match": {
-				"account_id": account_id, date: {
+				"account_id": account_id,
+				"pool": pool,
+				date: {
 					$gte: new Date(dateFrom),
 					$lt: dateTo2
 				}
@@ -31,7 +33,7 @@ const getDRewards = async (network, account_id, group, dateFrom, dateTo) => {
 
 	return {
 		labels: dRewards.map((data) => data._id), datasets: [{
-			label: "Near",
+			label: pool,
 			data: dRewards.map((data) => data.totalAmount.toString()),
 			borderColor: 'rgba(94, 48, 235, 0.8)',
 			backgroundColor: 'rgba(94, 48, 235, 0.8)',
@@ -42,7 +44,7 @@ const getDRewards = async (network, account_id, group, dateFrom, dateTo) => {
 export const getMyRewards = async (req, res) => {
 	try {
 		const group = ['%Y', '%Y-%m', '%Y-%m-%d'].includes(req.body.group) ? req.body.group : '%Y-%m-%d %H:%M';
-		const dRewards = await getDRewards(req.body.network, req.body.account_id, group, req.body.dateFrom, req.body.dateTo);
+		const dRewards = await getDRewards(req.body.network, req.body.account_id, req.body.pool, group, req.body.dateFrom, req.body.dateTo);
 		res.send({ dRewards });
 	} catch (e) {
 		console.log(e);
