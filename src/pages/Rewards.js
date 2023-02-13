3c9b681fc55ebc66bfcf671528951e9724a40b9f
andriy-bs <andriy.beskyd@gmail.com>
2023-02-13 09:31:55 +0200

    Add pool reward charts

--- src/pages/Rewards.js
+++ src/pages/Rewards.js
@@ -1,4 +1,4 @@
-import {Button, Container, Typography, Box, TextField} from "@mui/material";
+import {Button, Container, Typography, Box, TextField, Divider} from "@mui/material";
 import {useEffect, useRef, useState} from "react";
 import {nearConfig} from "../helpers/nearConfig";
 import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
@@ -30,49 +30,83 @@ ChartJS.register(
 );
 
 const Rewards = ({ wallet, isSignedIn }) => {
-	const [rewards, setRewards] = useState({ labels: [], datasets: [] });
-	const [group, setGroup] = useState("");
-	const chartRef = useRef(null);
-	const [dateFrom, setDateFrom] = useState(dayjs(new Date().setDate(new Date().getDate() - 90)));
-	const [dateTo, setDateTo] = useState(dayjs(new Date()));
-
+	const [kuutamoPools, setKuutamoPools] = useState([]);
+	const [myPools, setMyPools] = useState([]);
 
 	useEffect(() => {
 		(async () => {
-			await getRewards(wallet.accountId, group, dateFrom, dateTo);
-		})();
-	}, [wallet, group, dateFrom, dateTo]);
+			const requestOptions = {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					network: nearConfig.networkId,
+					account_id: wallet.accountId
+				})
+			};
+			const mp = await fetch(
+				nearConfig.backendUrl + "pools", requestOptions
+			).then(async response => {
+				return response.json();
+			}).then(data => {
+				const pools = [];
+				for (const v of data.myPools) {
+					pools.push(v.pool_id);
+				}
+				return (pools);
+			}).catch(error => {
+				console.error('There was an error!', error);
+			});
 
-	const getRewards = async (account_id, group, dateFrom, dateTo) => {
-		const requestOptions = {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({
-				network: nearConfig.networkId,
-				account_id,
-				dateFrom,
-				dateTo,
-				group
-			})
-		};
-		await fetch(
-			nearConfig.backendUrl + "rewards", requestOptions
-		).then(async response => {
-			const rs = await response.json();
-			console.log(rs.dRewards);
-			setRewards(rs.dRewards);
-		}).catch(error => {
-			console.error('There was an error!', error);
-		});
-	};
+			const kp = await fetch(nearConfig.backendUrl + 'validators.' + nearConfig.networkId + '.json').then(response => {
+				return response.json();
+			}).then(data => {
+				const pools = [];
+				for (const v of data) {
+					if (v.is_enabled && !mp.includes(v.account_id)) pools.push(v.account_id);
+				}
+				return (pools);
+			}).catch(err => {
+				console.error("Error Reading data " + err);
+			});
+			setMyPools(mp);
+			setKuutamoPools(kp);
+		})();
+
+	}, [wallet]);
+
 
 	if (!isSignedIn) {
 		wallet.signIn();
 		return false;
 	}
 
+
+	return <><Container>
+		<Typography component="h1" variant="h5" align="center">
+			Pool rewards
+		</Typography>
+		{ myPools.map((pool) =>
+			<RewardsLine key={ pool } pool={ pool } accountId={ wallet.accountId }/>
+		) }
+		<Divider/>
+		<Typography component="h1" variant="h5" align="center" pt={2}>
+			Delegations rewards
+		</Typography>
+		{ kuutamoPools.map((pool) =>
+			<RewardsLine key={ pool } pool={ pool } accountId={ wallet.accountId }/>
+		) }
+
+	</Container></>;
+};
+
+const RewardsLine = ({ pool, accountId }) => {
+	const [rewards, setRewards] = useState({ labels: [], datasets: [] });
+	const [group, setGroup] = useState("");
+	const chartRef = useRef(null);
+	const [dateFrom, setDateFrom] = useState(dayjs(new Date().setDate(new Date().getDate() - 90)));
+	const [dateTo, setDateTo] = useState(dayjs(new Date()));
+
 	const options = {
-		radius: 0,
 		plugins: {
 			zoom: {
 				zoom: {
@@ -88,55 +122,87 @@ const Rewards = ({ wallet, isSignedIn }) => {
 		}
 	};
 
+	useEffect(() => {
+		(async () => {
+			const getRewards = async (account_id, group, dateFrom, dateTo) => {
+				const requestOptions = {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({
+						network: nearConfig.networkId,
+						account_id,
+						pool,
+						dateFrom,
+						dateTo,
+						group
+					})
+				};
+				const r = await fetch(
+					nearConfig.backendUrl + "rewards", requestOptions
+				).then(async response => {
+					const rs = await response.json();
+					return (rs.dRewards);
+				}).catch(error => {
+					console.error('There was an error!', error);
+				});
+				return (r);
+			};
 
-	return <Container>
-		<Typography component="h1" variant="h5" align="center">
-			Delegations rewards
-		</Typography>
-		<Box align="right" sx={ { m: 2 } }>
-			<LocalizationProvider dateAdapter={ AdapterDayjs }>
-				<DesktopDatePicker
-					label="From"
-					inputFormat="MM/DD/YYYY"
-					value={ dateFrom }
-					onChange={ (date) => {
-						setDateFrom(date)
+			const r = await getRewards(accountId, group, dateFrom, dateTo);
+			setRewards(r);
+		})();
+	}, [accountId, pool, group, dateFrom, dateTo]);
+
+	return <>
+		{ rewards.labels.length > 0 ?
+			<>
+				<Box align="right" sx={ { m: 2 } }>
+					<LocalizationProvider dateAdapter={ AdapterDayjs }>
+						<DesktopDatePicker
+							label="From"
+							inputFormat="MM/DD/YYYY"
+							value={ dateFrom }
+							onChange={ (date) => {
+								setDateFrom(date)
+							} }
+							renderInput={ (params) => <TextField { ...params } size="small"/> }
+						/>
+						<DesktopDatePicker
+							label="To"
+							inputFormat="MM/DD/YYYY"
+							value={ dateTo }
+							onChange={ (date) => {
+								setDateTo(date)
+							} }
+							renderInput={ (params) => <TextField { ...params } size="small" sx={ { ml: 1 } }/> }
+						/>
+					</LocalizationProvider>
+					<Button onClick={ () => {
+						setGroup("");
+						chartRef.current.resetZoom()
 					} }
-					renderInput={ (params) => <TextField { ...params } size="small"/> }
-				/>
-				<DesktopDatePicker
-					label="To"
-					inputFormat="MM/DD/YYYY"
-					value={ dateTo }
-					onChange={ (date) => {
-						setDateTo(date)
+					        variant={ group === "" ? "contained" : "outlined" } size="small" sx={ { ml: 2 } }>Epoch</Button>
+					<Button onClick={ () => {
+						setGroup("%Y-%m-%d");
+						chartRef.current.resetZoom()
 					} }
-					renderInput={ (params) => <TextField { ...params } size="small" sx={ { ml: 1 } }/> }
-				/>
-			</LocalizationProvider>
-			<Button onClick={ () => {
-				setGroup("");
-				chartRef.current.resetZoom()
-			} }
-			        variant={ group === "" ? "contained" : "outlined" } size="small" sx={ { ml: 2 } }>Epoch</Button>
-			<Button onClick={ () => {
-				setGroup("%Y-%m-%d");
-				chartRef.current.resetZoom()
-			} }
-			        variant={ group === "%Y-%m-%d" ? "contained" : "outlined" } size="small" sx={ { ml: 1 } }>Day</Button>
-			<Button onClick={ () => {
-				setGroup("%Y-%m");
-				chartRef.current.resetZoom()
-			} } size="small" sx={ { ml: 1 } }
-			        variant={ group === "%Y-%m" ? "contained" : "outlined" }>Month</Button>
-			<Button onClick={ () => {
-				setGroup("%Y");
-				chartRef.current.resetZoom()
-			} } size="small" sx={ { ml: 1 } }
-			        variant={ group === "%Y" ? "contained" : "outlined" }>Year</Button>
-		</Box>
-		<Line data={ rewards } options={ options } ref={ chartRef }/>
-	</Container>;
-};
+					        variant={ group === "%Y-%m-%d" ? "contained" : "outlined" } size="small" sx={ { ml: 1 } }>Day</Button>
+					<Button onClick={ () => {
+						setGroup("%Y-%m");
+						chartRef.current.resetZoom()
+					} } size="small" sx={ { ml: 1 } }
+					        variant={ group === "%Y-%m" ? "contained" : "outlined" }>Month</Button>
+					<Button onClick={ () => {
+						setGroup("%Y");
+						chartRef.current.resetZoom()
+					} } size="small" sx={ { ml: 1 } }
+					        variant={ group === "%Y" ? "contained" : "outlined" }>Year</Button>
+				</Box>
+				<Line data={ rewards } options={ options } ref={ chartRef }/>
+			</> :
+			<></>
+		}
+	</>
+}
 
 export default Rewards;
