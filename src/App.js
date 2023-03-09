022d9d7582286757b27fcf592b29498f38b56fd6
andriy-bs <andriy.beskyd@gmail.com>
2023-03-09 10:35:04 +0200

    Сhange the footer

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function App({ isSignedIn, wallet }) {
 		<BrowserRouter>
 			<ConfirmProvider>
 				<Box sx={ { display: 'flex', zIndex: (theme) => theme.zIndex.drawer + 1, minHeight: `calc(100vh - 40px)` } }>
-					{ isSignedIn ? <NavBar isSignedIn={ isSignedIn } wallet={ wallet } drawerWidth={ drawerWidth }/> : <></>}
+					{ isSignedIn ? <NavBar isSignedIn={ isSignedIn } wallet={ wallet } drawerWidth={ drawerWidth }/> : <></> }
 					<Box component="main" sx={ { flexGrow: 1, p: 3, pt: 8, width: { sm: `calc(100% - ${ drawerWidth }px)` } } }>
 						<Toolbar/>
 						<Routes>
@@ -30,7 +30,7 @@ export default function App({ isSignedIn, wallet }) {
 						</Routes>
 					</Box>
 				</Box>
-				<Box sx={ { textAlign: "center", width: '100%', height: '40px' } }>© 2023 Sephora USA, Inc. All rights
+				<Box sx={ { textAlign: "center", width: '100%', height: '40px' } }>© 2023 kuutamo, Inc. All rights
 					reserved</Box>
 			</ConfirmProvider>
 		</BrowserRouter>
