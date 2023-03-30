7d3fc900bb8e599821954b27ea9886b02ea1ad2b
baliubakh <74013378+baliubakh@users.noreply.github.com>
2023-03-30 16:15:12 +0300

    fix: Fixed console error

---
+++ src/ui/components/ModalWrapper.jsx
@@ -0,0 +1,77 @@
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  useTheme,
+} from '@mui/material';
+import React from 'react';
+import ArrowLeftIcon from '../../svg/arrow-left';
+
+const ModalWrapper = ({ isOpen, onClose, title, children }) => {
+  const theme = useTheme();
+  return (
+    <Dialog open={isOpen} fullWidth maxWidth="sm">
+      <Button
+        sx={{
+          color: 'text.primary',
+          textTransform: 'none',
+          width: 'fit-content',
+          margin: '18px 0 0 18px',
+          fontSize: { lg: '16px', xl: '18px' },
+        }}
+        onClick={onClose}
+      >
+        <Box
+          sx={{
+            width: '28px',
+            height: '28px',
+            [theme.breakpoints.down('xl')]: {
+              width: '24px',
+              height: '24px',
+            },
+
+            marginRight: '11.5px',
+            color: 'primary.main',
+          }}
+        >
+          <ArrowLeftIcon />
+        </Box>
+        back
+      </Button>
+      <DialogContent sx={{ padding: 0 }}>
+        {title && (
+          <DialogTitle
+            id="alert-dialog-title"
+            sx={{
+              marginBlock: '14px 1em',
+              padding: '0',
+              textAlign: 'center',
+              fontWeight: 500,
+              lineHeight: {
+                xl: '52px',
+                lg: '48px',
+                md: '44px',
+                sm: '38px',
+                xs: '32px',
+              },
+              fontSize: {
+                xl: '52px',
+                lg: '48px',
+                md: '44px',
+                sm: '38px',
+                xs: '32px',
+              },
+            }}
+          >
+            {title}
+          </DialogTitle>
+        )}
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ModalWrapper;
